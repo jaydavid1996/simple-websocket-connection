b12f7e831e863fa845d4244186019756df890d6c
Jay Mark David <jaydavid1996@gmail.com>
2024-02-19 22:02:34 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { io } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'websocket-sample';
+  echo: any;
+  private socket = io('http://localhost:6002/',{
+    query: {token: environment.sampleToken }
+  }).connect();
+  
+  constructor() {
+    this.socket.on('connect', () => {
+        console.log('CONNECTED');
+    });
+    this.socket.on('transaction_changes', (data: any) => {
+        console.log('MESSAGE', data);
+        
+    });
+    console.log(this.echo);
+  }
+
+  btnTransactionSave() {
+    this.socket.emit('transaction_update', 
+      {
+        barangay_id: 1,
+        transaction_id: 1
+      }
+    );
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  sampleToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL3YxL2xvZ2luIiwiaWF0IjoxNzA3Mzk4NzE4LCJleHAiOjE3MDc0MDIzMTgsIm5iZiI6MTcwNzM5ODcxOCwianRpIjoiOWNubW9UeXFIc2FRRDR2TiIsInN1YiI6IjU3NDMyNzJiLWU4ZWYtNGZmNS05YmYxLTY5YjJmOWM1OGE2NyIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJyb2xlIjpbImFkbWluIl19.ej9HEGxwbw0b2MeiNYEhIvrC15VpPVnQBAoxSDAgsIQ"
 };
 
 /*
